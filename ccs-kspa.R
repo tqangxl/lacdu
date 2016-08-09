d7a5876c42d579a0417dba6b9f51588d79de1590
Peter W Setter <peter.w.setter@gmail.com>
2016-08-09 14:12:16 -0600

    New KSPA orders

--- ccs-kspa.R
+++ ccs-kspa.R
@@ -35,7 +35,7 @@ num.calls <- nrow(afterschool)
 
 # Make calls
 # Reference to TWIML file with instructions
-call.orders <- 'https://handler.twilio.com/twiml/EH350c11faa89d921d9299947c6cf890a1'
+call.orders <- 'https://handler.twilio.com/twiml/EH56709c45c2257c8bafc795db138c5190'
 load('.twilio-kspa.RData')
 office.email <- 'psetter@kippcolorado.org; data@climb.kippcolorado.org'
 #if(num.calls > 0) source('3-call.R')
