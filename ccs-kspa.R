82640e0ff062571ada93cc1218494d7a468560d2
Peter W Setter <peter.w.setter@gmail.com>
2016-08-10 12:01:05 -0600

    Turned on calls for KSPA

--- ccs-kspa.R
+++ ccs-kspa.R
@@ -38,7 +38,7 @@ num.calls <- nrow(afterschool)
 call.orders <- 'https://handler.twilio.com/twiml/EH56709c45c2257c8bafc795db138c5190'
 load('.twilio-kspa.RData')
 office.email <- 'psetter@kippcolorado.org; data@climb.kippcolorado.org'
-#if(num.calls > 0) source('3-call.R')
+if(num.calls > 0) source('3-call.R')
 
 # Email report
 #staff.email <- 'kspa_staff@kippcolorado.org'
