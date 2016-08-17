2498130a3a3a6301266d70f7d66a6cdd14fc552a
Peter W Setter <peter.w.setter@gmail.com>
2016-08-17 14:12:57 -0600

    Removed emailing Peter in event of failure and fixed no data issue for emailing

--- 4-email.R
+++ 4-email.R
@@ -4,10 +4,12 @@
 # Input afterschool
 
 # Convert afterschool into an html table
-afterschool.html <- afterschool.email %>%
-    htmlTable::htmlTable(col.rgroup = c("none", "#EFEFF0"))
+
 
 if(num.calls > 0) {
+    afterschool.html <- afterschool.email %>%
+        htmlTable::htmlTable(col.rgroup = c("none", "#EFEFF0"))
+    
     send_message(mime(from = 'noreply@climb.kippcolorado.org',
                       to = staff.email,
                       subject = paste('Consequence List', Sys.Date())) %>%
